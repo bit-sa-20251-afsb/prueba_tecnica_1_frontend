dc75fe771da428c58fca4dc4b3d78b55be372c72
AndresFSerranoPC <andresfserrano1@gmail.com>
2025-02-27 19:32:56 -0500

    adding environment to services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { ListUsersComponent } from './components/list-users/list-users.component
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 
 export const routes: Routes = [
-    { title: 'Lista de usuarios', path: 'list-users', component: ListUsersComponent},
+    { title: 'Lista de usuarios', path: 'list-users', component: ListUsersComponent, },
     { title: 'Registrar usuario', path: 'register', component: RegisterUserComponent}];
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    isProduction: true,
+    apiUrl: 'http://52.91.34.126:3005/api',
+};
